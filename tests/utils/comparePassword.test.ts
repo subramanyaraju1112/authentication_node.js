358806e99dfb6bf10fb3998651dc7205914c2e0c
subramanyaraju1112 <subramanyaraju1112@gmail.com>
2026-07-23 12:45:33 +0530

    updates: test cases - comparePassword

---
+++ tests/utils/comparePassword.test.ts
@@ -0,0 +1,31 @@
+import comparePassword from "../../src/utils/comparePassword";
+import hashPassword from "../../src/utils/hashPassword";
+
+describe("compare password", () => {
+    test("should return true for a valid password", async () => {
+        const password = "Password@123";
+        const hashedPassword = await hashPassword(password);
+
+        const isValid = await comparePassword(password, hashedPassword);
+
+        expect(isValid).toBe(true);
+    })
+
+    test("should return false for an invalid password", async () => {
+        const password = "Password@123";
+        const hashedPassword = await hashPassword(password);
+
+        const isValid = await comparePassword("wrongPassword", hashedPassword);
+
+        expect(isValid).toBe(false)
+    })
+
+    test("should return a boolean", async () => {
+        const password = "Password@123";
+        const hashedPassword = await hashPassword(password);
+
+        const result = await comparePassword(password, hashedPassword);
+
+        expect(typeof result).toBe("boolean")
+    })
+})
